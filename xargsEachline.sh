57841735076cb5b32abb2ee69c4a8e548450d1ee
Alberto Cubeddu <acubeddu87@gmail.com>
2016-05-04 09:45:01 +1000

    How to use each line of the txt

---
+++ xargsEachline.sh
@@ -0,0 +1,8 @@
+# From the man xargs:
+# -I replstr
+#             Execute utility for each input line, replacing one or more occurrences of replstr in up to replacements (or 5 if no -R flag is specified) arguments to utility with the entire line of input.  The
+#             resulting arguments, after replacement is done, will not be allowed to grow beyond 255 bytes; this is implemented by concatenating as much of the argument containing replstr as possible, to the
+#             constructed arguments to utility, up to 255 bytes.  The 255 byte limit does not apply to arguments to utility which do not contain replstr, and furthermore, no replacement will be done on
+#             utility itself.  Implies -x.
+
+cat fileName | xargs -I{} cp {} directory/
