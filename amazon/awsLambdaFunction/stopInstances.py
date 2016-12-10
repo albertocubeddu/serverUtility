36dcb034a5c12c902f46bb74a323b4f207bce90b
alberto <alberto.cubeddu@creditorwatch.com.au>
2016-12-10 20:26:39 +1100

    Start and stopping instance using lambda

---
+++ amazon/awsLambdaFunction/stopInstances.py
@@ -0,0 +1,23 @@
+import boto3
+
+region = 'ap-southeast-2'
+instancesContainer = []
+
+ec2 = boto3.resource('ec2', region_name=region,  api_version='2016-04-01')
+ec2Client = boto3.client('ec2', region_name=region,  api_version='2016-04-01')
+
+#Retrieve all the instances
+instances = ec2.instances.filter(Filters = [{'Name':'tag:Name', 'Values':['testing*']}, {'Name':'instance-state-name', 'Values':['running']}])
+
+for instance in instances:
+    for tag in instance.tags:
+        if (tag['Key'] == 'Name'):
+            instancesContainer.append(instance.instance_id)
+            print (instance.instance_id, tag['Value'])
+
+def lambda_handler(event, context):
+    if instancesContainer:
+        ec2Client.stop_instances(InstanceIds=instancesContainer)
+        print 'stopped your instances: ' + str(instancesContainer)
+    else:
+        print 'No instance need to be stopped'
